4f0f4e410b8a34ee3ebc815c4f7b0ec7be7bbbc3
Ryan Fleck <ryan.fleck@protonmail.com>
2020-06-16 20:25:41 -0400

    Change Postgres security option.

--- backend/database.js
+++ backend/database.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgresql",
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: dev ? false : true,
+      rejectUnauthorized: false,
     },
   },
 });
