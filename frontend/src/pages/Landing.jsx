251e2ebe573edf8604bdeddfe759a9fd4c24e3f3
Ryan Fleck <ryan.fleck@protonmail.com>
2020-06-18 14:52:11 -0400

    Write landing page.

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb, Typography, Row, Col } from "antd";
+import {
+  Layout,
+  Menu,
+  Carousel,
+  Typography,
+  Spin,
+  Row,
+  Col,
+  Button,
+  Space,
+} from "antd";
 import axios from "axios";
 import axiosRetry from "axios-retry";
+import { Link, Redirect, redirectTo, navigate } from "@reach/router";
+import { AuditOutlined, FilterOutlined } from "@ant-design/icons";
 
 // Axios Config
 axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
@@ -16,21 +28,69 @@ export default class Landing extends React.Component {
     super(props);
     this.state = {
       title: "Landing",
+      get: false,
     };
+
+    this.goToEditor = this.goToEditor.bind(this);
+    this.goToQuery = this.goToQuery.bind(this);
   }
 
   componentDidMount() {
     /* Attempt GET */
     axios.get("/api/get").then((res) => {
-      console.log("GETting data...");
       this.setState({ get: true, getData: res.data });
     });
+  }
+
+  goToEditor() {
+    console.log("Redirecting to editor...");
+    navigate("/editor");
+  }
+
+  goToQuery() {
+    console.log("Redirecting to query...");
+    navigate("/query");
   }
 
   render() {
     return (
       <div>
-        <Title>{this.state.title}</Title>
+        <Layout
+          className={"mainLayout"}
+          style={{
+            padding: "1em",
+            textAlign: "center",
+          }}
+        >
+          <Title
+            size="large"
+            style={{ paddingTop: "20vh", paddingBottom: "0" }}
+          >
+            Xalgorithms
+          </Title>
+          <Text style={{ paddingTop: "0", paddingBottom: "10vh" }}>
+            Integrated Rule Author and User System
+          </Text>
+          <div>
+            <Space>
+              <Button
+                type="primary"
+                size="large"
+                icon={<AuditOutlined />}
+                onClick={this.goToEditor}
+              >
+                Write Rules
+              </Button>
+              <Button
+                size="large"
+                icon={<FilterOutlined />}
+                onClick={this.goToQuery}
+              >
+                Use Rules
+              </Button>
+            </Space>
+          </div>
+        </Layout>
       </div>
     );
   }
